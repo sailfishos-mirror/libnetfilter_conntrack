807e041a74f05e1266176cf9a28687388cdf6cb3
Kevin Cernekee <cernekee@chromium.org>
2016-08-14 13:59:01 -0700

    src: Make the library compile under clang
    clang treats "char buffer[size]" inside a union as VLAIS unless |size|
    is const:
    
    src/conntrack/api.c:992:8: error: fields must have a constant size: 'variable length array in structure' extension will never be supported
                    char buffer[size];
                         ^
    
    Signed-off-by: Kevin Cernekee <cernekee@chromium.org>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/conntrack/api.c
+++ src/conntrack/api.c
@@ -955,7 +955,7 @@ int nfct_query(struct nfct_handle *h,
 	       const enum nf_conntrack_query qt,
 	       const void *data)
 {
-	size_t size = 4096;	/* enough for now */
+	const size_t size = 4096;	/* enough for now */
 	union {
 		char buffer[size];
 		struct nfnlhdr req;
@@ -987,7 +987,7 @@ int nfct_send(struct nfct_handle *h,
 	      const enum nf_conntrack_query qt,
 	      const void *data)
 {
-	size_t size = 4096;	/* enough for now */
+	const size_t size = 4096;	/* enough for now */
 	union {
 		char buffer[size];
 		struct nfnlhdr req;
