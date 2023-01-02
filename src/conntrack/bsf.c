sailfishos-mirror/libnetfilter_conntrack

commit b245e4092c5a7f09729e64868a42e13f48ac5db8

src: allow to use nfct handler for conntrack and expectations at the same time
This patch re-works the callback handling to allow the use the same socket
to send/receive commands and listen to events of both conntrack and
expectation subsystems. Now you can register one callback for conntrack
and one for expectation with the same handler with no problems (before
this patch, this was not possible, you required two different handlers).

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/conntrack/bsf.c
@@ -8,6 +8,7 @@
 #include "internal/internal.h"
 #include "internal/stack.h"
 #include <linux/filter.h>
+#include <stddef.h>		/* offsetof */
 
 #ifndef SKF_AD_NLATTR
 #define SKF_AD_NLATTR		12
@@ -213,6 +214,33 @@ nfct_bsf_jump_to(struct sock_filter *this, int line, int pos)
 	memcpy(&this[pos], &__code, sizeof(__code));
 	return NEW_POS(__code);
 };
+
+/* this helps to skip messages coming from the ctnetlink expectation subsys. */
+static int
+bsf_cmp_subsys(struct sock_filter *this, int pos, u_int8_t subsys)
+{
+	struct sock_filter __code[] = {
+		[0] = {
+			/* X = offset to nlh->nlmsg_type */
+			.code	= BPF_LDX|BPF_IMM,
+			.k	= offsetof(struct nlmsghdr, nlmsg_type),
+		},
+		[1] = {
+			/* A = skb->data[X+k:B] (subsys_id) */
+			.code	= BPF_LD|BPF_B|BPF_IND,
+			.k	= sizeof(u_int8_t),
+		},
+		[2] = {
+			/* A == subsys ? jump +1 : accept */
+			.code	= BPF_JMP|BPF_JEQ|BPF_K,
+			.k	= subsys,
+			.jt	= 1,
+			.jf	= 0,
+		},
+	};
+	memcpy(&this[pos], &__code, sizeof(__code));
+	return NEW_POS(__code);
+}
 
 static int
 add_state_filter_cta(struct sock_filter *this,
@@ -584,6 +612,8 @@ int __setup_netlink_socket_filter(int fd, struct nfct_filter *f)
 
 	memset(bsf, 0, sizeof(bsf));
 
+	j += bsf_cmp_subsys(&bsf[j], j, NFNL_SUBSYS_CTNETLINK);
+	j += nfct_bsf_ret_verdict(bsf, NFCT_FILTER_ACCEPT, j);
 	j += bsf_add_proto_filter(f, &bsf[j]);
 	j += bsf_add_saddr_ipv4_filter(f, &bsf[j]);
 	j += bsf_add_daddr_ipv4_filter(f, &bsf[j]);

README
@@ -62,6 +62,10 @@ library only provides the new API that solves former deficiencies. Thus, make
 sure you use recent versions of libnetfilter_conntrack and, in case that
 you are using the old API, consider porting your application to the new one.
 
+Since libnetfilter_conntrack >= 0.9.1, you can use the same handler obtained
+via nfct_open() to register conntrack and expectation callbacks (before this
+version, this was not possible).
+
 = References =
 
 [1] Pablo Neira Ayuso. Netfilter's Connection Tracking System:

include/internal/object.h
@@ -16,7 +16,8 @@ struct nfct_handle {
 	struct nfnl_subsys_handle	*nfnlssh_exp;
 
 	/* callback handler for the new API */
-	struct nfnl_callback		nfnl_cb;
+	struct nfnl_callback		nfnl_cb_ct;
+	struct nfnl_callback		nfnl_cb_exp;
 
 	int			(*cb)(enum nf_conntrack_msg_type type, 
 				      struct nf_conntrack *ct,

src/Makefile.am
@@ -15,4 +15,4 @@ libnetfilter_conntrack_la_LIBADD = conntrack/libnfconntrack.la \
 				   ${LIBNFNETLINK_LIBS}
 libnetfilter_conntrack_la_LDFLAGS = -Wc,-nostartfiles -lnfnetlink -ldl 	\
 				    -version-info $(LIBVERSION)
-libnetfilter_conntrack_la_SOURCES = main.c
+libnetfilter_conntrack_la_SOURCES = main.c callback.c

src/callback.c
@@ -1,0 +1,111 @@
+/*
+ * (C) 2006-2010 by Pablo Neira Ayuso <pablo@netfilter.org>
+ *
+ * This software may be used and distributed according to the terms
+ * of the GNU General Public License, incorporated herein by reference.
+ */
+
+#include "internal/internal.h"
+
+static int __parse_message(const struct nlmsghdr *nlh)
+{
+	u_int16_t type = NFNL_MSG_TYPE(nlh->nlmsg_type);
+	u_int16_t flags = nlh->nlmsg_flags;
+	int ret = NFCT_T_UNKNOWN;
+
+	switch(type) {
+	case IPCTNL_MSG_CT_NEW: /* same value for IPCTNL_MSG_EXP_NEW. */
+		if (flags & (NLM_F_CREATE|NLM_F_EXCL))
+			ret = NFCT_T_NEW;
+		else
+			ret = NFCT_T_UPDATE;
+		break;
+	case IPCTNL_MSG_CT_DELETE: /* same value for IPCTNL_MSG_EXP_DELETE. */
+		ret = NFCT_T_DESTROY;
+		break;
+	}
+	return ret;
+}
+
+int __callback(struct nlmsghdr *nlh, struct nfattr *nfa[], void *data)
+{
+	int ret = NFNL_CB_STOP;
+	unsigned int type;
+	struct nf_conntrack *ct = NULL;
+	struct nf_expect *exp = NULL;
+	struct __data_container *container = data;
+	u_int8_t subsys = NFNL_SUBSYS_ID(nlh->nlmsg_type);
+
+	if (nlh->nlmsg_len < NLMSG_LENGTH(sizeof(struct nfgenmsg))) {
+		errno = EINVAL;
+		return NFNL_CB_FAILURE;
+	}
+	type = __parse_message(nlh);
+	if (!(type & container->type))
+		return NFNL_CB_CONTINUE;
+
+	switch(subsys) {
+	case NFNL_SUBSYS_CTNETLINK:
+		ct = nfct_new();
+		if (ct == NULL)
+			return NFNL_CB_FAILURE;
+
+		__parse_conntrack(nlh, nfa, ct);
+
+		if (container->h->cb) {
+			ret = container->h->cb(type, ct, container->data);
+		} else if (container->h->cb2) {
+			ret = container->h->cb2(nlh, type, ct,
+						container->data);
+		}
+		break;
+	case NFNL_SUBSYS_CTNETLINK_EXP:
+		exp = nfexp_new();
+		if (exp == NULL)
+			return NFNL_CB_FAILURE;
+
+		__parse_expect(nlh, nfa, exp);
+
+		if (container->h->expect_cb) {
+			ret = container->h->expect_cb(type, exp,
+						      container->data);
+		} else if (container->h->expect_cb2) {
+			ret = container->h->expect_cb2(nlh, type, exp,
+						       container->data);
+		}
+		break;
+	default:
+		errno = ENOTSUP;
+		ret = NFNL_CB_FAILURE;
+		break;
+	}
+
+	switch(ret) {
+	case NFCT_CB_FAILURE:
+		if (ct)
+			free(ct);
+		if (exp)
+			free(exp);
+		ret = NFNL_CB_FAILURE;
+		break;
+	case NFCT_CB_STOP:
+		if (ct)
+			free(ct);
+		if (exp)
+			free(exp);
+		ret = NFNL_CB_STOP;
+		break;
+	case NFCT_CB_CONTINUE:
+		if (ct)
+			free(ct);
+		if (exp)
+			free(exp);
+		ret = NFNL_CB_CONTINUE;
+		break;
+	case NFCT_CB_STOLEN:
+		ret = NFNL_CB_CONTINUE;
+		break;
+	}
+out:
+	return ret;
+}

src/conntrack/Makefile.am
@@ -4,7 +4,7 @@ AM_CFLAGS = -Wall ${LIBNFNETLINK_CFLAGS}
 
 noinst_LTLIBRARIES = libnfconntrack.la 
 
-libnfconntrack_la_SOURCES = api.c callback.c \
+libnfconntrack_la_SOURCES = api.c \
 			    getter.c setter.c \
 			    parse.c build.c \
 			    snprintf.c \

src/conntrack/api.c
@@ -226,17 +226,17 @@ int nfct_callback_register(struct nfct_handle *h,
 	container->type = type;
 	container->data = data;
 
-	h->nfnl_cb.call = __callback;
-	h->nfnl_cb.data = container;
-	h->nfnl_cb.attr_count = CTA_MAX;
+	h->nfnl_cb_ct.call = __callback;
+	h->nfnl_cb_ct.data = container;
+	h->nfnl_cb_ct.attr_count = CTA_MAX;
 
 	nfnl_callback_register(h->nfnlssh_ct, 
 			       IPCTNL_MSG_CT_NEW,
-			       &h->nfnl_cb);
+			       &h->nfnl_cb_ct);
 
 	nfnl_callback_register(h->nfnlssh_ct,
 			       IPCTNL_MSG_CT_DELETE,
-			       &h->nfnl_cb);
+			       &h->nfnl_cb_ct);
 
 	return 0;
 }
@@ -253,11 +253,11 @@ void nfct_callback_unregister(struct nfct_handle *h)
 	nfnl_callback_unregister(h->nfnlssh_ct, IPCTNL_MSG_CT_DELETE);
 
 	h->cb = NULL;
-	free(h->nfnl_cb.data);
+	free(h->nfnl_cb_ct.data);
 
-	h->nfnl_cb.call = NULL;
-	h->nfnl_cb.data = NULL;
-	h->nfnl_cb.attr_count = 0;
+	h->nfnl_cb_ct.call = NULL;
+	h->nfnl_cb_ct.data = NULL;
+	h->nfnl_cb_ct.attr_count = 0;
 }
 
 /**
@@ -300,17 +300,17 @@ int nfct_callback_register2(struct nfct_handle *h,
 	container->type = type;
 	container->data = data;
 
-	h->nfnl_cb.call = __callback;
-	h->nfnl_cb.data = container;
-	h->nfnl_cb.attr_count = CTA_MAX;
+	h->nfnl_cb_ct.call = __callback;
+	h->nfnl_cb_ct.data = container;
+	h->nfnl_cb_ct.attr_count = CTA_MAX;
 
 	nfnl_callback_register(h->nfnlssh_ct, 
 			       IPCTNL_MSG_CT_NEW,
-			       &h->nfnl_cb);
+			       &h->nfnl_cb_ct);
 
 	nfnl_callback_register(h->nfnlssh_ct,
 			       IPCTNL_MSG_CT_DELETE,
-			       &h->nfnl_cb);
+			       &h->nfnl_cb_ct);
 
 	return 0;
 }
@@ -327,11 +327,11 @@ void nfct_callback_unregister2(struct nfct_handle *h)
 	nfnl_callback_unregister(h->nfnlssh_ct, IPCTNL_MSG_CT_DELETE);
 
 	h->cb2 = NULL;
-	free(h->nfnl_cb.data);
+	free(h->nfnl_cb_ct.data);
 
-	h->nfnl_cb.call = NULL;
-	h->nfnl_cb.data = NULL;
-	h->nfnl_cb.attr_count = 0;
+	h->nfnl_cb_ct.call = NULL;
+	h->nfnl_cb_ct.data = NULL;
+	h->nfnl_cb_ct.attr_count = 0;
 }
 
 /**

src/conntrack/callback.c
@@ -1,55 +1,0 @@
-/*
- * (C) 2006 by Pablo Neira Ayuso <pablo@netfilter.org>
- *
- * This software may be used and distributed according to the terms
- * of the GNU General Public License, incorporated herein by reference.
- */
-
-#include "internal/internal.h"
-
-int __callback(struct nlmsghdr *nlh, struct nfattr *nfa[], void *data)
-{
-	int ret = NFNL_CB_STOP;
-	unsigned int type;
-	struct nf_conntrack *ct;
-	int len = nlh->nlmsg_len;
-	struct __data_container *container = data;
-
-	len -= NLMSG_LENGTH(sizeof(struct nfgenmsg));
-	if (len < 0)
-		return NFNL_CB_CONTINUE;
-
-	type = __parse_message_type(nlh);
-	if (!(type & container->type))
-		return NFNL_CB_CONTINUE;
-
-	ct = nfct_new();
-	if (!ct)
-		return NFNL_CB_CONTINUE;
-
-	__parse_conntrack(nlh, nfa, ct);
-
-	if (container->h->cb)
-		ret = container->h->cb(type, ct, container->data);
-	else if (container->h->cb2)
-		ret = container->h->cb2(nlh, type, ct, container->data);
-
-	switch(ret) {
-	case NFCT_CB_FAILURE:
-		free(ct);
-		ret = NFNL_CB_FAILURE;
-		break;
-	case NFCT_CB_STOP:
-		free(ct);
-		ret = NFNL_CB_STOP;
-		break;
-	case NFCT_CB_CONTINUE:
-		free(ct);
-		ret = NFNL_CB_CONTINUE;
-		break;
-	case NFCT_CB_STOLEN:
-		ret = NFNL_CB_CONTINUE;
-		break;
-	}
-	return ret;
-}

commit 43d7b069f43e047b05f0e0ad5d5528afedc4ac1d

conntrack: fix BPF code for filtering on big-endian architectures
The BPF for checking the subsystem ID looks for it in the righthand byte of
`nlh->nlmsg_type`.  However, it will only be there on little-endian archi-
tectures.  The result is that on big-endian architectures the subsystem ID
doesn't match, all packets are immediately accepted, and all filters are
ignored.

Link: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=896716
Fixes: b245e4092c5a ("src: allow to use nfct handler for conntrack and expectations at the same time")
Signed-off-by: Jeremy Sowden <jeremy@azazel.net>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/conntrack/bsf.c
@@ -9,6 +9,7 @@
 
 #include "internal/internal.h"
 #include "internal/stack.h"
+#include <endian.h>
 #include <linux/filter.h>
 #include <stddef.h>		/* offsetof */
 
@@ -301,10 +302,14 @@ bsf_cmp_subsys(struct sock_filter *this, int pos, uint8_t subsys)
 		[1] = {
 			/* A = skb->data[X+k:B] (subsys_id) */
 			.code	= BPF_LD|BPF_B|BPF_IND,
+#if BYTE_ORDER == BIG_ENDIAN
+			.k	= 0,
+#else
 			.k	= sizeof(uint8_t),
+#endif
 		},
 		[2] = {
-			/* A == subsys ? jump +1 : accept */
+			/* A == subsys ? jump + 1 : accept */
 			.code	= BPF_JMP|BPF_JEQ|BPF_K,
 			.k	= subsys,
 			.jt	= 1,